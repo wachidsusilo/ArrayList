2d551bfa86b7b941ef3ee4cc948031578c8b921a
Wachid Susilo <50637964+wachidsusilo@users.noreply.github.com>
2023-01-22 21:03:02 +0700

    Refactor private member and function name

--- src/ArrayList.h
+++ src/ArrayList.h
@@ -80,24 +80,31 @@ class ArrayList {
     };
 
     ArrayList()
-        : m_Data(NULL), m_Size(0), m_Capacity(0) {
-        reallocate(2);
+        : m_Data(NULL),
+          m_Size(0),
+          m_Capacity(0) {
+        _reallocate(2);
     }
 
-    ArrayList(const ArrayList& other) : ArrayList() {
-        if (reallocate(other.m_Capacity)) {
-            m_Size = other.m_Size;
+    ArrayList(const ArrayList& other)
+        : ArrayList() {
+        if (_reallocate(other.m_Capacity)) {
+            m_Size            = other.m_Size;
             const size_t size = m_Size * sizeof(T);
             memcpy(m_Data, other.m_Data, size);
         }
     }
 
-    ArrayList(ArrayList&& other) noexcept : ArrayList() {
-        swap(other);
+    ArrayList(ArrayList&& other) noexcept
+        : ArrayList() {
+        _swap(other);
     }
 
-    ArrayList(T arr[], size_t len) : m_Data(NULL), m_Size(0), m_Capacity(0) {
-        if (reallocate(len + len / 2)) {
+    ArrayList(T arr[], size_t len)
+        : m_Data(NULL),
+          m_Size(0),
+          m_Capacity(0) {
+        if (_reallocate(len + len / 2)) {
             for (size_t i = 0; i < len; i++) {
                 add(arr[i]);
             }
@@ -105,42 +112,42 @@ class ArrayList {
     }
 
     template <typename... Args>
-    ArrayList(Args... args) : ArrayList() {
-        if (reallocate(sizeof...(args) + sizeof...(args) / 2)) {
+    ArrayList(Args... args)
+        : ArrayList() {
+        if (_reallocate(sizeof...(args) + sizeof...(args) / 2)) {
             int dummy[] = {(add(args), 0)...};
             (void)dummy;
         }
     }
 
     ~ArrayList() {
-        deallocate();
+        _deallocate();
     }
 
     bool add(const T& element) {
         if (m_Size >= m_Capacity) {
-            if (!reallocate(m_Capacity + m_Capacity / 2)) return false;
+            if (!_reallocate(m_Capacity + m_Capacity / 2)) return false;
         }
         m_Data[m_Size] = element;
         m_Size++;
         return true;
     }
 
-    bool add(T*& element) {
+    bool add(T&& element) {
         if (m_Size >= m_Capacity) {
-            if (!reallocate(m_Capacity + m_Capacity / 2)) return false;
+            if (!_reallocate(m_Capacity + m_Capacity / 2)) return false;
         }
-        m_Data[m_Size] = *element;
+        m_Data[m_Size] = element;
         m_Size++;
         return true;
     }
 
     void remove(const T& element) {
-        removeByIndex(indexOf(element));
+        removeAt(indexOf(element));
     }
 
-    void removeByIndex(size_t index) {
+    void removeAt(size_t index) {
         if (index < 0) return;
-        m_Data[index].~T();
         m_Size--;
         for (size_t i = index; i < m_Size; i++) {
             m_Data[i] = m_Data[i + 1];
@@ -149,17 +156,24 @@ class ArrayList {
 
 #if defined(ESP32) || defined(ESP8266)
     void removeIf(std::function<bool(T)> predicate) {
-#else
-    template <typename Callable>
-    void removeIf(Callable predicate) {
-#endif
         for (size_t i = 0; i < m_Size; i++) {
             if (predicate(m_Data[i])) {
-                removeByIndex(i);
+                removeAt(i);
                 i--;
             }
         }
     }
+#else
+    template <typename Callable>
+    void removeIf(Callable predicate) {
+        for (size_t i = 0; i < m_Size; i++) {
+            if (predicate(m_Data[i])) {
+                removeAt(i);
+                i--;
+            }
+        }
+    }
+#endif
 
     bool contains(const T& element) {
         for (size_t i = 0; i < m_Size; i++) {
@@ -170,29 +184,30 @@ class ArrayList {
 
 #if defined(ESP32) || defined(ESP8266)
     bool contains(std::function<bool(T)> predicate) {
-#else
-    template <typename Callable>
-    bool contains(Callable predicate) {
-#endif
         for (size_t i = 0; i < m_Size; i++) {
             if (predicate(m_Data[i])) return true;
         }
         return false;
     }
-
-    int indexOf(const T& element) {
+#else
+    template <typename Callable>
+    bool contains(Callable predicate) {
         for (size_t i = 0; i < m_Size; i++) {
-            if (m_Data[i] == element) return i;
+            if (predicate(m_Data[i])) return true;
         }
-        return -1;
+        return false;
     }
+#endif
+
+int indexOf(const T& element) {
+    for (size_t i = 0; i < m_Size; i++) {
+        if (m_Data[i] == element) return i;
+    }
+    return -1;
+}
 
 #if defined(ESP32) || defined(ESP8266)
     int indexOf(std::function<bool(T)> predicate) {
-#else
-    template <typename Callable>
-    int indexOf(Callable predicate) {
-#endif
         for (size_t i = 0; i < m_Size; i++) {
             if (predicate(m_Data[i])) return i;
         }
@@ -200,17 +215,33 @@ class ArrayList {
     }
 
     void clear() {
-        deallocate();
-        reallocate(2);
+        _deallocate();
+        _reallocate(2);
+    }
+#else
+    template <typename Callable>
+    int indexOf(Callable predicate) {
+        for (size_t i = 0; i < m_Size; i++) {
+            if (predicate(m_Data[i])) return i;
+        }
+        return -1;
     }
 
-    const T& get(const size_t& index, const T& defVal) const {
-        if (index >= m_Size) return defVal;
+    void clear() {
+        _deallocate();
+        _reallocate(2);
+    }
+#endif
+
+    const T get(const size_t& index, const T& defaultValue) const {
+        if (index >= m_Size) {
+            return defaultValue;
+        }
         return m_Data[index];
     }
 
-    T& get(const size_t& index, const T& defVal) {
-        if (index >= m_Size) return const_cast<T&>(defVal);
+    T get(const size_t& index, const T& defaultValue) {
+        if (index >= m_Size) return const_cast<T&>(defaultValue);
         return m_Data[index];
     }
 
@@ -224,8 +255,8 @@ class ArrayList {
 
     ArrayList& operator=(const ArrayList& other) {
         m_Size = 0;
-        if (reallocate(other.m_Capacity)) {
-            m_Size = other.m_Size;
+        if (_reallocate(other.m_Capacity)) {
+            m_Size            = other.m_Size;
             const size_t size = m_Size * sizeof(T);
             memcpy(m_Data, other.m_Data, size);
         }
@@ -233,62 +264,86 @@ class ArrayList {
     }
 
     ArrayList& operator=(ArrayList&& other) noexcept {
-        swap(other);
+        _swap(other);
         return *this;
     }
 
 #if defined(ESP32) || defined(ESP8266)
     void forEach(std::function<bool(T&, size_t)> predicate) {
-#else
-    template <typename Callable>
-    void forEach(Callable predicate) {
-#endif
         for (size_t i = 0; i < m_Size; i++) {
             if (!predicate(m_Data[i], i)) break;
         }
     }
+#else
+    template <typename Callable>
+    void forEach(Callable predicate) {
+        for (size_t i = 0; i < m_Size; i++) {
+            if (!predicate(m_Data[i], i)) break;
+        }
+    }
+#endif
 
 #if defined(ESP32) || defined(ESP8266)
     template <typename E = T>
     ArrayList<E> map(std::function<E(T&, size_t)> predicate) {
-#else
-    template <typename E = T, typename Callable>
-    ArrayList<E> map(Callable predicate) {
-#endif
         ArrayList<E> buffer;
         for (size_t i = 0; i < m_Size; i++) {
             buffer.add(predicate(m_Data[i], i));
         }
         return buffer;
     }
+#else
+    template <typename E = T, typename Callable>
+    ArrayList<E> map(Callable predicate) {
+        ArrayList<E> buffer;
+        for (size_t i = 0; i < m_Size; i++) {
+            buffer.add(predicate(m_Data[i], i));
+        }
+        return buffer;
+    }
+#endif
 
 #if defined(ESP32) || defined(ESP8266)
     ArrayList filter(std::function<bool(T&)> predicate) {
-#else
-    template <typename Callable>
-    ArrayList filter(Callable predicate) {
-#endif
         ArrayList buffer;
         for (size_t i = 0; i < m_Size; i++) {
             if (predicate(m_Data[i])) buffer.add(m_Data[i]);
         }
         return buffer;
     }
+#else
+    template <typename Callable>
+    ArrayList filter(Callable predicate) {
+        ArrayList buffer;
+        for (size_t i = 0; i < m_Size; i++) {
+            if (predicate(m_Data[i])) buffer.add(m_Data[i]);
+        }
+        return buffer;
+    }
+#endif
 
 #if defined(ESP32) || defined(ESP8266)
     void sort(std::function<bool(T&, T&)> predicate) {
-#else
-    template <typename Callable>
-    void sort(Callable predicate) {
-#endif
         for (size_t i = 1; i < m_Size; i++) {
             for (size_t j = i; j > 0 && predicate(m_Data[j - 1], m_Data[j]); j--) {
-                T tmp = m_Data[j - 1];
+                T tmp         = m_Data[j - 1];
                 m_Data[j - 1] = m_Data[j];
-                m_Data[j] = tmp;
+                m_Data[j]     = tmp;
             }
         }
     }
+#else
+    template <typename Callable>
+    void sort(Callable predicate) {
+        for (size_t i = 1; i < m_Size; i++) {
+            for (size_t j = i; j > 0 && predicate(m_Data[j - 1], m_Data[j]); j--) {
+                T tmp         = m_Data[j - 1];
+                m_Data[j - 1] = m_Data[j];
+                m_Data[j]     = tmp;
+            }
+        }
+    }
+#endif
 
     void sort() {
         sort([](T a, T b) -> bool { return a > b; });
@@ -296,8 +351,8 @@ class ArrayList {
 
     void reverse() {
         for (size_t i = 0; i < m_Size / 2; i++) {
-            T buffer = m_Data[i];
-            m_Data[i] = m_Data[m_Size - 1 - i];
+            T buffer               = m_Data[i];
+            m_Data[i]              = m_Data[m_Size - 1 - i];
             m_Data[m_Size - 1 - i] = buffer;
         }
     }
@@ -310,11 +365,11 @@ class ArrayList {
         return Iterator(m_Data + m_Size);
     }
 
-    size_t size() {
+    size_t size() const {
         return m_Size;
     }
 
-    bool isEmpty() {
+    bool isEmpty() const {
         return m_Size == 0;
     }
 
@@ -323,7 +378,7 @@ class ArrayList {
     size_t m_Size;
     size_t m_Capacity;
 
-    bool reallocate(size_t newCapacity) {
+    bool _reallocate(size_t newCapacity) {
         T* newBlock = new T[newCapacity];
         if (newBlock) {
             if (newCapacity < m_Size) {
@@ -333,31 +388,31 @@ class ArrayList {
                 newBlock[i] = m_Data[i];
             }
             delete[] m_Data;
-            m_Data = newBlock;
+            m_Data     = newBlock;
             m_Capacity = newCapacity;
             return true;
         }
         return false;
     }
 
-    void deallocate() {
+    void _deallocate() {
         delete[] m_Data;
-        m_Data = NULL;
-        m_Size = 0;
+        m_Data     = NULL;
+        m_Size     = 0;
         m_Capacity = 0;
     }
 
-    void swap(ArrayList& other) {
-        T* _data = m_Data;
-        size_t _size = m_Size;
+    void _swap(ArrayList& other) {
+        T* _data         = m_Data;
+        size_t _size     = m_Size;
         size_t _capacity = m_Capacity;
 
-        m_Data = other.m_Data;
-        m_Size = other.m_Size;
+        m_Data     = other.m_Data;
+        m_Size     = other.m_Size;
         m_Capacity = other.m_Capacity;
 
-        other.m_Data = _data;
-        other.m_Size = _size;
+        other.m_Data     = _data;
+        other.m_Size     = _size;
         other.m_Capacity = _capacity;
     }
 };
